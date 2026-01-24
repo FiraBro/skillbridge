c1e7ef00acc7f739d2940eb83f613b8e33869d79
FiraBro <jemalfiragos@gmail.com>
2026-01-24 23:33:09 +0300

    add file to it

--- apps/api/src/modules/auth/auth.controller.js
+++ apps/api/src/modules/auth/auth.controller.js
@@ -1,5 +1,41 @@
-// Auth controller
+import * as authService from "./auth.service.js";
+import { validateRegister, validateLogin } from "./auth.schema.js";
+import { success } from "../utils/apiResponse.js";
+export const register = async (req, res, next) => {
+  try {
+    validateRegister(req.body);
+    const result = await authService.register(req.body);
+    res.status(201).json(success(result, "User registered"));
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports = {
-  // methods
+export const login = async (req, res, next) => {
+  try {
+    validateLogin(req.body);
+    const result = await authService.login(req.body);
+    res.json(success(result, "Login successful"));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const forgotPassword = async (req, res, next) => {
+  try {
+    await authService.forgotPassword(req.body.email);
+    res.json(success(null, "If email exists, reset link sent"));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const resetPassword = async (req, res, next) => {
+  try {
+    const { token, password } = req.body;
+    await authService.resetPassword(token, password);
+    res.json(success(null, "Password reset successful"));
+  } catch (err) {
+    next(err);
+  }
 };

5c55387b924efd2fd1e1fd54cf6f97096b46b55d
FiraBro <jemalfiragos@gmail.com>
2026-01-24 23:43:15 +0300

    add file to it

--- apps/api/src/modules/middlewares/auth.middleware.js
+++ apps/api/src/modules/middlewares/auth.middleware.js
@@ -1,6 +1,21 @@
-// Auth middleware
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
+import { ApiError } from "../utils/apiError.js";
 
-module.exports = (req, res, next) => {
-  // auth logic
-  next();
+export const requireAuth = (req, res, next) => {
+  const header = req.headers.authorization;
+
+  if (!header?.startsWith("Bearer ")) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, env.JWT_ACCESS_SECRET);
+    req.user = payload;
+    next();
+  } catch {
+    throw new ApiError(401, "Invalid token");
+  }
 };

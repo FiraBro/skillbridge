44cfe3fee8138ba00e951b85c12694b2283d680f
FiraBro <jemalfiragos@gmail.com>
2026-01-26 15:27:50 +0300

    validation schema

--- apps/api/src/modules/posts/post.schema.js
+++ apps/api/src/modules/posts/post.schema.js
@@ -1,5 +1,9 @@
-// Post schema
+import { z } from "zod";
 
-module.exports = {
-  // schema
-};
+export const createPostSchema = z.object({
+  title: z.string().min(5),
+  markdown: z.string().min(10),
+  tags: z.array(z.string()).max(5),
+});
+
+export const updatePostSchema = createPostSchema.partial();
